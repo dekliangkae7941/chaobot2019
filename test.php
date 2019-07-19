5294b4fbbf3ece7b52148bdc877bff65a5fbca76
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-19 17:22:53 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,12 @@
 <?php
+    $Setup_Server = '127.0.0.1';
+    $Setup_User = 'root';
+    $Setup_Pwd = '79417941';
+    $Setup_Database = 'chatbot';
+    mysql_connect($Setup_Server,$Setup_User,$Setup_Pwd);
+    mysql_query("chatbot $Setup_Database");
+    mysql_query("SET NAMES UTF8");
+
 echo '<div style="font-size:14px">';
 
 echo '--------------------------------------------------------------------';
