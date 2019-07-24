403c5f5e81f329ea968ce987e72648ca6c43ed93
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-24 15:17:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "085021231";
+$password = "";
 $dbconnect = "chatbot";
 $conn = mysqli_connect($servername, $username, $password, $dbconnect);
 // Check connection
