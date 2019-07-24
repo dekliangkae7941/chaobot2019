2e5f182af2167e913f4a6bb58a013272c05a389b
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-24 10:31:37 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
     $Setup_Server = '127.0.0.1';
     $Setup_User = 'root';
-    $Setup_Pwd = '79417941';
+    $Setup_Pwd = '0850212315';
     $Setup_Database = 'chatbot';
     mysql_connect($Setup_Server,$Setup_User,$Setup_Pwd);
     mysql_query("chatbot $Setup_Database");
