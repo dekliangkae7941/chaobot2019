cc3bd07fca9cc4e4e710cd416c3617658177c397
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-30 11:20:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   $userID = $jsonData["events"][0]["source"]["userId"];
   $text = $jsonData["events"][0]["message"]["text"];
   $timestamp = $jsonData["events"][0]["timestamp"];
-  $servername = "localhost";
+  $servername = "	localhostchatbot";
   $username = "dekliangkae";
   $password = "0967358315";
   $dbname = "chatbot_chaokaset";
