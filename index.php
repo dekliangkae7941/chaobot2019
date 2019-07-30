74faf4b3754050c207d4cff75b12e34974137bfc
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-30 16:35:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,39 @@
   $text = $jsonData["events"][0]["message"]["text"];
   $timestamp = $jsonData["events"][0]["timestamp"];*/
   
-  $link = mysqli_connect("localhost", "root", "", "chatbot_chaokaset");
 
-  if (!$link) {
-      echo "Error: Unable to connect to MySQL." . PHP_EOL;
-      echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-      echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-      exit;
-  }
 
-  echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-  echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-  mysqli_close($link);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "chatbot_chaokaset";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
+
   /*$servername = "localhost";
   $username = "dekliangkae";
   $password = "0967358315";
